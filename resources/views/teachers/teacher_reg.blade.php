2d2bd17bf78d59fc11b2c0b48af8aed43712b558
Unknown <earnpucca06@gmail.com>
2017-05-02 16:19:33 +0700

    เปลี่ยน data list หน้าสมัครครูกับนักเรียน เป็น dropdown และ เพิ่ม validate rule

--- resources/views/teachers/teacher_reg.blade.php
+++ resources/views/teachers/teacher_reg.blade.php
@@ -25,7 +25,7 @@
                     {!! csrf_field() !!}
                     @if(count($errors))
                     <div class="alert alert-danger">
-                        <strong>Whoops!</strong> There were some problems with your input.
+                        <strong>กรุณาใส่ข้อมูลให้ครบถ้วน!</strong>
                         <br/>
                         <ul>
                             @foreach($errors->all() as $error)
@@ -112,7 +112,7 @@
                         <div class="col-sm-4 col-md-4 text-left">
                             <div class="form-group {{ $errors->has('tel') ? 'has-error' : '' }}" id="tel">
                                 <input type="text" name="tel" class="form-control"
-                                placeholder="กรุณาระบุ เบอร์บ้าน" value="{{ old('tel') }}" maxlength="10" onKeyUp="inputDigitsTel(this);">
+                                placeholder="กรุณาระบุ เบอร์บ้าน" value="{{ old('tel') }}" maxlength="9" onKeyUp="inputDigitsTel(this);">
                                 <span class="text-danger">{{ $errors->first('tel') }}</span>
                             </div>
                         </div>
@@ -143,46 +143,36 @@
                     <div class="row">
                         <span class="col-sm-2 col-md-2 text-right">จังหวัด:</span>
                         <div class="col-sm-4 col-md-4">
-                            <input list="opts_province" name="province_list"
-                                class="form-control"
-                                placeholder="กรุณาระบุ จังหวัด">
-                            <datalist id="opts_province">
+                            <select class="form-control" id="province_id" name="province_id">
+                                <option value="none">กรุณาเลือก จังหวัด</option>
                                 @foreach($prov as $prov_list)
-                                    <option id="{{ $prov_list->province_id }}"
-                                        value="{{ $prov_list->province_name }}">
+                                    <option value="{{ $prov_list->province_id }}">
+                                        {{ $prov_list->province_name }}
                                     </option>
                                 @endforeach
-                            </datalist>
-                            <input type="hidden" id="province_id" name="province_id">
+                            </select>
                         </div>
                         <span class="col-sm-2 col-md-2 text-right">เขต/อำเภอ:</span>
-                        <div class="col-sm-4 col-md-4 text-left">
-                            <input list="opts_district" name="district_list"
-                                class="form-control"
-                                placeholder="กรุณาระบุ เขต/อำเภอ"
-                                onclick="chkProvinceInput();">
-                            <datalist id="opts_district">
-                            </datalist>
-                            <input type="hidden" id="district_id" name="district_id">
+                        <div class="col-sm-4 col-md-4">
+                            <select class="form-control" id="district_id" name="district_id">
+                                <option value="none">กรุณาเลือก เขต/อำเภอ</option>
+                            </select>
                         </div>
                     </div></br>
                     <div class="row">
                         <span class="col-sm-2 col-md-2 text-right">แขวง/ตำบล:</span>
                         <div class="col-sm-4 col-md-4 text-left">
-                            <input list="opts_sub_district" name="sub_district_list"
-                                class="form-control"
-                                placeholder="กรุณาระบุ แขวง/ตำบล"
-                                onclick="chkDistrictInput();">
-                            <datalist id="opts_sub_district">
-                            </datalist>
-                            <input type="hidden" id="sub_district_id" name="sub_district_id">
+                            <select class="form-control" id="sub_district_id" name="sub_district_id">
+                                <option value="none">กรุณาเลือก แขวง/ตำบล</option>
+                            </select>
                         </div>
                         <span class="col-sm-2 col-md-2 text-right">รหัสไปรษณีย์:</span>
                         <div class="col-sm-4 col-md-4 text-left">
                             <div class="form-group {{ $errors->has('postcode') ? 'has-error' : '' }}" id="postcode" >
                                 <input type="text" name="postcode" class="form-control"
                                     placeholder="กรุณาระบุ รหัสไปรษณีย์" value="{{ old('postcode') }}"
-                                    onclick="chkSubDistrictInput();" onKeyUp="inputDigits(this);">
+                                    onclick="chkSubDistrictInput();" onKeyUp="inputDigits(this);"
+                                    maxlength="5">
                                 <span class="text-danger">{{ $errors->first('postcode') }}</span>
                             </div>
                         </div>
@@ -236,134 +226,60 @@
 
 <script type="text/javascript">
 
-    $(document).ready(function(){
-        var prov_list_id;
-        var prov_list_value;
-        var dist_list_id;
-        var dist_list_value;
-        var sub_dist_list_id;
-        var sub_dist_list_value;
-        $('input[name=province_list]').on('input',function() {
-            var selectedOption = $('option[value="'+$(this).val()+'"]');
-            if(selectedOption.length) {
-                prov_list_id = selectedOption.attr('id');
-                prov_list_value = selectedOption.attr('value');
-                $('input[name=province_list]').val(prov_list_value);
-                $('input[name=province_id]').val(prov_list_id);
+    $("#province_id").change(function () {
+        var prov_id = $('#province_id :selected').val();
 
-                getDistrict(prov_list_id);
+        var select = $("#district_id");
+        select.empty();
 
-            }
-        });
+        var select_sub_dist = $("#sub_district_id");
+        select_sub_dist.empty();
+        select_sub_dist.append($('<option/>', {
+            value: "none",
+            text: "กรุณาเลือก แขวง/ตำบล"
+        }));
 
-        $('input[name=district_list]').on('input',function() {
-            var selectedOption = $('option[value="'+$(this).val()+'"]');
-            if(selectedOption.length) {
-                dist_list_id = selectedOption.attr('id');
-                dist_list_value = selectedOption.attr('value');
-                $('input[name=district_list]').val(dist_list_value);
-                $('input[name=district_id]').val(dist_list_id);
-
-                getSubDistrict(prov_list_id, dist_list_id);
-
-            }
-        });
-
-        $('input[name=sub_district_list]').on('input',function() {
-            var selectedOption = $('option[value="'+$(this).val()+'"]');
-            if(selectedOption.length) {
-                sub_dist_list_id = selectedOption.attr('id');
-                sub_dist_list_value = selectedOption.attr('value');
-                $('input[name=sub_district_list]').val(sub_dist_list_value);
-                $('input[name=sub_district_id]').val(sub_dist_list_id);
-            }
-        });
-
-        $('input[name=district_list]').on( 'keyup', function( e ) {
-            if( e.which == 9 ) {
-                //console.log( e.target.href );
-                chkProvinceInput();
-            }
-        } );
-
-        $('input[name=sub_district_list]').on( 'keyup', function( e ) {
-            if( e.which == 9 ) {
-                //console.log( e.target.href );
-                chkDistrictInput();
-            }
-        } );
-
-        $('input[name=postcode]').on( 'keyup', function( e ) {
-            if( e.which == 9 ) {
-                //console.log( e.target.href );
-                chkSubDistrictInput();
-            }
-        } );
-    });
-
-    function chkProvinceInput() {
-        var prov_id
-        prov_id = $('input[name=province_id]').val();
-
-        if(prov_id.length == 0) {
-            $('input[name=province_list]').val("");
-        }
-    }
-
-    function chkDistrictInput() {
-        var dist_id
-        dist_id = $('input[name=district_id]').val();
-
-        if(dist_id.length == 0) {
-            $('input[name=district_list]').val("");
-        }
-    }
-
-    function chkSubDistrictInput() {
-        var sub_dist_id
-        sub_dist_id = $('input[name=sub_district_id]').val();
-        if(sub_dist_id.length == 0) {
-            $('input[name=sub_district_list]').val("");
-        }
-    }
-
-    function getDistrict(prov_id) {
         $.ajax({
             type: 'GET',
             url: "{{ url('/districts') }}",
             data: { prov_id: prov_id },
             dataType: 'json',
             success: function (data) {
-                var select = $("#opts_district");
-                select.empty();
+                select.append($('<option/>', {
+                    value: "none",
+                    text: "กรุณาเลือก เขต/อำเภอ"
+                }));
                 $.each(data, function (index, dist_data) {
                     select.append($('<option/>', {
-                        id: dist_data.district_id,
-                        value: dist_data.district_name
+                        value: dist_data.district_id,
+                        text: dist_data.district_name,
                     }));
                 });
             }
         });
-    }
+    });
 
-    function getSubDistrict(prov_id, dist_id) {
+    $("#district_id").change(function () {
+        var prov_id = $('#province_id :selected').val();
+        var dist_id = $('#district_id :selected').val();
+
         $.ajax({
             type: 'GET',
             url: "{{ url('/sub_districts') }}",
             data: { prov_id: prov_id, dist_id: dist_id },
             dataType: 'json',
             success: function (data) {
-                var select = $("#opts_sub_district");
+                var select = $("#sub_district_id");
                 select.empty();
                 $.each(data, function (index, sub_dist_data) {
                     select.append($('<option/>', {
-                        id: sub_dist_data.sub_district_id,
-                        value: sub_dist_data.sub_district_name
+                        value: sub_dist_data.sub_district_id,
+                        text: sub_dist_data.sub_district_name
                     }));
                 });
             }
         });
-    }
+    });
 
     function inputDigitsMobile(sensor){
         var regExp = /[0-9]$/;
