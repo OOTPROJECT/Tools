a7c31803fff364a816c29c6e7dd4c07b7ddf6373
Unknown <earnpucca06@gmail.com>
2017-05-02 10:57:36 +0700

    Add <br>.

--- resources/views/course/course_enroll.blade.php
+++ resources/views/course/course_enroll.blade.php
@@ -52,12 +52,12 @@
                           <div class="row">
                               <span class="col-sm-2 col-md-1 text-right">ชื่อ:</span>
                                   <div class="col-sm-2 col-md-2 text-left">
-                                      <input type="text" id="std_fname" name="std_fname">
+                                      <input type="text" id="std_fname" name="std_fname"><br>
                                       <span id="span-name"class="text-right" style="display:none; color:red">กรุณาระบุชื่อ</span>
                                   </div>
                               <span class="col-sm-2 col-md-1 text-right">นามสกุล:</span>
                                   <div class="col-sm-2 col-md-2 text-left">
-                                      <input type="text" id="std_lname" name="std_lname">
+                                      <input type="text" id="std_lname" name="std_lname"><br>
                                       <span id="span-lname"class="text-right" style="display:none; color:red">กรุณาระบุนามสกุล</span>
                                   </div>
                                   <div class="col-sm-4 col-md-2 text-left">
